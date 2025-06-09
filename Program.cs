707e6645a7f93f5813de246ebbde834db3922b96
Theivasri D <theivasri1@gmail.com>
2025-06-09 16:14:46 +0530

    refactot : Add due amount functionality

--- Program.cs
+++ Program.cs
@@ -8,3 +8,20 @@ Magazine m1 = new Magazine("Peace", "111", 68);
 m1.Showinfo();
 DVD d1 = new DVD("Silent", "TS", 143);
 d1.Showinfo();
+
+Console.WriteLine("------------Pay Due Amount for extra days----------");
+Console.WriteLine("Number of days : ");
+int days = int.Parse(Console.ReadLine());
+var due = 15;
+var fine = 0;
+if (days > due)
+{
+    var diff = days - due;
+    for (int i = 1; i <= diff; i++)
+    {
+        fine += 2;
+    }
+}
+Console.WriteLine($"The Fine amount you have to pay  : {fine}");
+Console.ReadKey();
+Console.WriteLine("Press any key to exit..");
